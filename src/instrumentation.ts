bb133e1d8b43ac9af90f2259bb28204bf80f38b6
Kaenova Mahendra Auditama <kaenova@gmail.com>
2025-11-21 10:34:59 +0700

    Adding application insight

---
+++ src/instrumentation.ts
@@ -0,0 +1,28 @@
+// (src/)instrumentation.ts
+import type { SpanExporter } from '@opentelemetry/sdk-trace-base';
+import { registerOTel } from '@vercel/otel';
+
+export async function register() {
+  let traceExporter: SpanExporter | undefined;
+
+  if (!process.env.APPLICATION_INSIGHTS_CONNECTION_STRING) {
+    console.warn('APPLICATION_INSIGHTS_CONNECTION_STRING is not set. Skipping OpenTelemetry registration.');
+    return
+  }
+
+  if (!process.env.APPLICATION_INSIGHTS_SERVICE_NAME) {
+    console.warn('APPLICATION_INSIGHTS_SERVICE_NAME is not set. Skipping OpenTelemetry registration.');
+    return
+  }
+
+  if (process.env.NEXT_RUNTIME === 'nodejs') {
+    const { AzureMonitorTraceExporter } = await import('@azure/monitor-opentelemetry-exporter');
+    traceExporter = new AzureMonitorTraceExporter({
+      connectionString: process.env.APPLICATION_INSIGHTS_CONNECTION_STRING,
+      // you can read from ENV if you prefer to
+      // connectionString: process.env.APP_INSIGHTS_CONNECTION_STRING,
+    });
+  }
+
+  registerOTel({ serviceName: process.env.APPLICATION_INSIGHTS_SERVICE_NAME, traceExporter });
+}
